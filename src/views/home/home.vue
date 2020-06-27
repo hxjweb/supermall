206bf88abb2f2694a3238671ffb2a8c7c4484b11
hxj <1844659454@qq.com>
2020-06-27 10:26:34 +0800

    首页本周流行

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -7,19 +7,22 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommends="recommends"></home-recommend>
+    <home-feature></home-feature>
   </div>
 </template>
 <script>
 import navBar from "../../components/navBar";
 import homeSwiper from "@/views/home/homeSwiper/homeSwiper.vue";
 import homeRecommend from "@/views/home/homeRecommend.vue";
+import homeFeature from "@/views/home/homeFeature.vue";
 import { getHomeMultiData } from "../../views/home/home.js";
 
 export default {
   components: {
     navBar,
     homeSwiper,
-    homeRecommend
+    homeRecommend,
+    homeFeature
   },
   data() {
     return {
---
+++ src/views/home/homeFeature.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home_feature">
+    <a>
+      <img src="../../assets/img/recommend_bg.jpg" />
+    </a>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.home_feature img {
+  width: 100%;
+}
+</style>
